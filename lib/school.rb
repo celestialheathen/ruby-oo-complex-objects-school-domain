fea84deaf707948498e75d50877c051ffc37ab0d
celestialheathen <jingchaoyu@hotmail.com>
2021-01-16 12:48:06 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+class School 
+
+    attr_accessor :roster 
+    attr_reader :name
+
+    def initialize(name)
+        @name = name 
+        @roster = {}
+    end 
+
+    def add_student(name, grade)
+        if @roster[grade] == nil 
+            @roster[grade] = []
+            @roster[grade].push(name)
+        else 
+            @roster[grade].push(name)
+        end 
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.collect { |key, value|
+            @roster[key] = value.sort
+    }
+    @roster
+    end
+
+end
+
+
+
+
